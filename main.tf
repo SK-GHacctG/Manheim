17f24637389d44b641a7a192e8db1736f75db718
Sarowar Kamal, Google <90433021+SK-GHacctG@users.noreply.github.com>
2024-03-08 00:52:09 -0500

    adding file via upload

---
+++ main.tf
@@ -0,0 +1,21 @@
+#
+# AWS account access needed; if not an authorized user in the account, 
+# the access_key and Secret_key are needed
+#
+provider "aws" {
+  region = var.myregion
+  access_key = "value..."
+  secret_key = "value ..."
+}
+
+# Backend definition
+#
+terraform {
+  required_version = ">= 0.13"
+
+  backend "s3" {
+    bucket = "sktfstatebucket"
+    key    = "skproject1/terraform.tfstate"
+    region = "us-east-1"
+  }
+}
