7dd912a55e9da2dfa3a27168bf086e206cffa897
Sarowar Kamal, Google <90433021+SK-GHacctG@users.noreply.github.com>
2024-03-08 23:59:56 -0500

    Update variables.tf
    updated some comments

--- variables.tf
+++ variables.tf
@@ -1,17 +1,18 @@
 #
 #
 # Region
+#
 variable "myregion" {
   type = string
   default = "us-east-1"
   description = "The AWS Region"
 }
 
-# VPC
+# VPC - Assuming this is the VPC provided
 #
 variable "var_vpc_id" {
   type = string
-  default = "vpc-0b4f32ef53a79ea6c"
+  default = "vpc-0b4f32ef53a79ea6c"        # The id is not a real one.
   description = "Assuming this is the vpc id provided"
 }
 
@@ -19,19 +20,16 @@ variable "var_vpc_id" {
 #
 variable "var_subnet_id1" {
   type = string
-  default = "subnet-09f78bd306fce4033"      # this subnet id is not a real one
+  default = "subnet-09f78bd306fce4033"      # Assuming this is a subnet provided; the id is not a real one.
   description = "Assuming this is a public subnet"
-  Tags {
-
-  }
 }
 
 # Public subnet | Subnet id 2
 #
 variable "var_subnet_id2" {
   type = string
-  default = "subnet-0e3a476b68b9bbcb4"      # this subnet id is not a real one
+  default = "subnet-0e3a476b68b9bbcb4"      # This subnet id is not a real one; assuming this is a subnet given.
   description = "Assuming this is another public subnet within the VPC"
 }
 
-#more subnets may be needed
+# More subnets may be needed in real network
